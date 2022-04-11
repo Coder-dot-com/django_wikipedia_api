3391cbcb2fe7d10306c91f25e20f51d3e8b027a9
usman17.s@hotmail.com <usman17.s@hotmail.com>
2022-04-11 21:28:30 +0100

    Add try/except to search function

--- django_wikipedia_api/django_wikipedia_api/utils.py
+++ django_wikipedia_api/django_wikipedia_api/utils.py
@@ -5,19 +5,22 @@ from bs4 import BeautifulSoup
 
 
 def search_wikipedia(search_term):
-    site = requests.get(f"https://en.wikipedia.org/w/index.php?search={search_term}&title=Special:Search&profile=advanced&fulltext=1&ns0=1").text
-    soup = BeautifulSoup(site, 'html.parser')
+    try:
+        site = requests.get(f"https://en.wikipedia.org/w/index.php?search={search_term}&title=Special:Search&profile=advanced&fulltext=1&ns0=1").text
+        soup = BeautifulSoup(site, 'html.parser')
 
-    results = soup.select(selector="#bodyContent .mw-search-result .searchresult")
-    if not results:
-        return ["No results found"]
+        results = soup.select(selector="#bodyContent .mw-search-result .searchresult")
+        if not results:
+            return ["No results found"]
 
 
-    else:
-        list_of_results = [result.getText() for result in results]
-        print(list_of_results)
-        print(list_of_results[0])
-        print(list_of_results[1])
-        return list_of_results
+        else:
+            list_of_results = [result.getText() for result in results]
+            print(list_of_results)
+            print(list_of_results[0])
+            print(list_of_results[1])
+            return list_of_results
+    except:
+        return ["An unexplained error occured"]
 
 
